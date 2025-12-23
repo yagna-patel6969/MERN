eb8543f6bcbebb496cccfea1f8c33407c4c9bfe7
yagna-patel6969 <pyagna6969@gmail.com>
2025-12-23 22:34:22 +0530

    string

---
+++ js/PDO/01.js
@@ -0,0 +1,14 @@
+let str = "  JS  ";
+console.log(str.trim().length);
+
+let str1 = "JavaScript";
+console.log(str.slice(4, 10));
+
+console.log("5" + 2);
+
+console.log("5" - 2);
+
+console.log("Hello".charAt(10));
+
+console.log("Hello"[10]);
+
